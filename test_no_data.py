38e4c075262c8e7942c96689985ef9f9662e75c3
Jessica (My) Vu <123513822+jfm-code@users.noreply.github.com>
2024-05-30 15:14:08 +0700

    Initial idea about testing pins

--- test_no_data.py
+++ test_no_data.py
@@ -3,6 +3,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 import unittest
 from set_up import Base
+from selenium.common.exceptions import NoSuchElementException
 
 class Test_NoData(unittest.TestCase):
     @classmethod
@@ -23,6 +24,7 @@ class Test_NoData(unittest.TestCase):
         try:
             self.logger.info("Started counting the model components in model page")
             self.driver.find_element(By.CSS_SELECTOR, "div[class='flex h-full items-center w-full']").click()
+            self.logger.debug("Clicked the Select Model button")
             models = self.driver.find_elements(By.CSS_SELECTOR, "a[class='mr-2 w-full']")
             assert (len(models) > 0)
             self.logger.info("Successfully tested the number of model components in model page")
@@ -37,3 +39,18 @@ class Test_NoData(unittest.TestCase):
             self.logger.info("Successfully tested the number of prototype components in home page")
         except Exception as e:
             self.logger.error(f"Failed to load the prototype components in home page: {e}")
+            
+    def test_redPin_exist(self):
+        try:
+            self.driver.find_element(By.CSS_SELECTOR, "div[class='flex h-full items-center w-full']").click() # Click the Select Model Button
+            self.logger.debug("Clicked the Select Model button")
+            self.driver.find_element(By.CSS_SELECTOR, "img[src='https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2FE-Car_Full_Vehicle.png?alt=media&token=9c9d4cb4-fee0-42e3-bbb1-7feaa407cc8e']").click()
+            self.logger.debug("Clicked the ACME Car (EV) v0.1 Model")
+            self.driver.find_element(By.XPATH, "//div/div[text()='Vehicle APIs']").click()
+            self.logger.debug("Clicked the Vehicle APIs button")
+            self.driver.find_element(By.CSS_SELECTOR, "div[style$='cursor: default; position: relative;']")
+            # Canvas that have pins will have this styling attribute
+            # If I have the coordinate of one pin then I can hover on it, this styling attribute only appears when we hover once
+            # Since I don't have the coord of the pin, this will always throw exception
+        except NoSuchElementException:
+            self.logger.error("Failed to load the red pins on the canvas")
